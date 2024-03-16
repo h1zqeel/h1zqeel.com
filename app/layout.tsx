27b42c5bfb2ca00ead2eba18449b2a40931c73b1
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2024-03-17 02:32:44 +1100

    add favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "h1zqeel.com",
     images: [
       {
-        url: "https://h1zqeel.com/og.png",
+        url: "https://h1zqeel.com/og.jpg",
         width: 1920,
         height: 1080,
       },
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
-    shortcut: "/favicon.png",
+    shortcut: "/favicon.jpg",
   },
 };
 const inter = Inter({
